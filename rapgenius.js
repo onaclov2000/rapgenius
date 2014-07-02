fea3589bfb07fb60d3ed25272de21732c22cb327
Tyson Bailey <onaclov2000+github@gmail.com>
2014-07-02 08:30:20 -0600

    Update rapgenius.js

--- rapgenius.js
+++ rapgenius.js
@@ -1,1 +1,14 @@
-asfdasfe
+// Currently not tested, but ready for work.
+ angular.module('music', []).factory('$music', ['$http', function($http) {
+      return { $artist : function (name,genre)
+                  {
+                          var method = 'POST';
+                          var url = "http://genius-api.com/ap/artistInfo";
+                          $http({method: method, url: url  + "name=" + artist + 
+                                                                           "genre=" + genre}).
+                          success(function(data, status) {}).
+                          error(function(data, status) {});   
+                  }
+            };
+    }
+  ]);
