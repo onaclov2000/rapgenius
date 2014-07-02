d656715bb1e9aea6ad4c42ceda9ac9c9728b683a
Tyson Bailey <onaclov2000+github@gmail.com>
2014-07-02 08:34:27 -0600

    Update rapgenius.js

--- rapgenius.js
+++ rapgenius.js
@@ -8,6 +8,24 @@
                                                                            "genre=" + genre}).
                           success(function(data, status) {}).
                           error(function(data, status) {});   
+                  },
+               $song : function (name,genre)
+                  {
+                          var method = 'POST';
+                          var url = "http://genius-api.com/ap/songInfo";
+                          $http({method: method, url: url  + "name=" + artist + 
+                                                                           "genre=" + genre}).
+                          success(function(data, status) {}).
+                          error(function(data, status) {});   
+                  },
+               $lyrics : function (link,genre)
+                  {
+                          var method = 'POST';
+                          var url = "http://genius-api.com/ap/lyricsInfo";
+                          $http({method: method, url: url  + "link=" + artist + 
+                                                                           "genre=" + genre}).
+                          success(function(data, status) {}).
+                          error(function(data, status) {});   
                   }
             };
     }
